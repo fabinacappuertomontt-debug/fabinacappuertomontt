aff1975e2bd11fe1a899adf5a6c4ff70552cccfc
fabinacappuertomontt-debug <fabinacappuertomontt@gmail.com>
2026-05-07 16:11:54 -0400

    Corregir arranque de Django en Azure

--- startup.sh
+++ startup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+export PYTHONPATH="/home/site/wwwroot/.python_packages/lib/site-packages:$PYTHONPATH"
+
 python manage.py collectstatic --noinput
-python manage.py migrate
-python -m gunicorn seguimiento.wsgi:application --bind=0.0.0.0:8000
+
+if [ "${RUN_MIGRATIONS:-false}" = "true" ]; then
+    python manage.py migrate --noinput
+fi
+
+exec python -m gunicorn seguimiento.wsgi:application --bind=0.0.0.0:${PORT:-8000}
